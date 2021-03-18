51e56c11acdbd2e8471bb3c29bd337a9c1463b29
Joaquim-Frances <quimifrances@gmail.com>
2021-03-18 21:29:21 +0100

    Played with useEffect

---
+++ src/components/02-useEffect/Message.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+export const Message = (  ) => {
+
+    const [coors, setCoors] = useState({x: 0, y: 0})
+
+    const { x, y } = coors;
+
+    const mouseMove = (e) => {
+        const coords = { x: e.x, y:e.y };
+        setCoors( coords );
+    }
+    
+    useEffect(() => {
+
+        window.addEventListener('mousemove', mouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', mouseMove);
+        }
+    }, [])
+
+    return (
+        <div>
+            <h3>Eres genial!!</h3>
+            <p>
+                x: {x} y: {y}
+            </p>
+        </div>
+    )
+}
--- src/components/02-useEffect/SimpleForm.jsx
+++ src/components/02-useEffect/SimpleForm.jsx
@@ -1,16 +1,66 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './effects.css';
+import { Message } from './Message';
 
 export const SimpleForm = () => {
 
-    useEffect( * );
+    const [form, setForm] = useState({
+        name:'',
+        email:'',
+    });
+
+    const {name, email} = form;
+
+    useEffect( ()=>{
+        console.log('Hey!');
+    } , [] );
+
+    useEffect( ()=>{
+        console.log('name cambió!');
+    } , [name] );
+
+    useEffect( ()=>{
+        console.log('email cambió!');
+    } , [email] );
+
+    const handleInputChange = ({target}) =>{
+        
+        setForm({
+            ...form,
+            [target.name]: target.value
+        });
+    }
 
     return (
         <div>
             <h1>Simple Form (useEffect)</h1>
             <hr/>
-
+            <div className='form-group'>
+                <input 
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Tu nombre"
+                    autoComplete="off"
+                    value={ name }
+                    onChange={ handleInputChange }
+                />
+            </div>
+            <div className='form-group'>
+                <input 
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="tu@email.dot"
+                    autoComplete="off"
+                    value={ email }
+                    onChange={ handleInputChange }
+                />
+            </div>
+            
+            { (name === 'Quim') && <Message />}
 
         </div>
+        
     )
 }
