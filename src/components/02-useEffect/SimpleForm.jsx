8261b36f424a4e5960f4147e54d38b9208f7c069
Joaquim-Frances <quimifrances@gmail.com>
2021-03-18 09:10:38 +0100

    Learning useEffect

---
+++ src/components/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import './effects.css';
+
+export const SimpleForm = () => {
+
+    useEffect( * );
+
+    return (
+        <div>
+            <h1>Simple Form (useEffect)</h1>
+            <hr/>
+
+
+        </div>
+    )
+}
