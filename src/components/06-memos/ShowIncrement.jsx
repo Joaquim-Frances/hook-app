db8a3c15fb0e4042e8472315cda372f009ebc3f3
Joaquim-Frances <quimifrances@gmail.com>
2021-03-22 22:51:02 +0100

    Learning useCallback Hook and useMemo

---
+++ src/components/06-memos/ShowIncrement.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+export const ShowIncrement = React.memo(({increment}) => {
+
+    console.log('Me volví a generar... :( ')
+    return (
+        <button className='btn btn-primary' onClick={()=>{increment(5)}}>
+            Incrementar
+        </button>
+    )
+})
+
+ShowIncrement.propType={
+    increment: PropTypes.func.isRequired,
+}
