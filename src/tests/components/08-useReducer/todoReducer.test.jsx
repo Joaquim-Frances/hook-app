703e8b60293237add75ea4f01294ab7f8c9304c5
Joaquim-Frances <quimifrances@gmail.com>
2021-03-31 18:46:35 +0200

    Testing useReducer

---
+++ src/tests/components/08-useReducer/todoReducer.test.jsx
@@ -0,0 +1,75 @@
+import { todoReducer } from "../../../components/08-useReducer/todoReducer"
+
+const initialTodos = [{
+    id: 1,
+    todo: 'Apendre a estar sol',
+    done: false,
+},
+{
+    id: 2,
+    todo: 'Tenir una bona economia',
+    done: false,
+}]
+
+
+describe('Pruebas en todoReducer', () => {
+
+
+    test('should return default state', () => {
+        
+        const state = todoReducer(initialTodos, {});
+        expect(state.length).toBe(2);
+        expect(state).toEqual(initialTodos);
+        
+    })
+
+    test('should have an added todo', () => {
+        
+        const newTodo = {
+            id: 3,
+            todo: 'Donarse de baixa de la Caixa',
+            done: false,
+        }
+        
+        const agregarTodoAction = {
+            type: 'add',
+            payload: newTodo,
+        }
+
+        const state = todoReducer(initialTodos, agregarTodoAction);
+    
+        expect(state.length).toBe(3);
+        expect(state).toEqual([...initialTodos, newTodo]);
+       
+    })
+
+    test('should delete todo', () => {
+
+        const oldTodo = {
+            id: 2,
+            todo: 'Tenir una bona economia',
+            done: false,
+        }
+
+        const deleteTodoAction = {
+            type: 'delete',
+            payload: oldTodo,
+        }
+        
+        const state = todoReducer(initialTodos, deleteTodoAction);
+        expect(state.length).toBe(2);
+    })
+
+    test('should toggle done/undone a todo', () => {
+
+        const toggleTodoAction = {
+            type: 'toggle',
+            payload: 2,
+        }
+        
+        const state = todoReducer(initialTodos, toggleTodoAction);
+        console.log(state);
+        expect(state[1].done).toBe(true);
+        
+    })
+})
